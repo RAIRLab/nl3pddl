7831fab98070a4813195c2352854845af01ef8f2
Vova956 <Vladimrvarga2019@gmail.com>
2025-10-13 14:12:17 -0400

    Fixed the generators and Added a new Tile domain

--- nl3pddl/problem_generators/__init__.py
+++ nl3pddl/problem_generators/__init__.py
@@ -7,6 +7,8 @@ from .flow import generate_problem as generate_flow_problem
 from .bookseller import generate_problem as generate_bookseller_problem
 from .hiking import generate_hiking_problem
 from .keygrid import generate_keygrid_problem
+from .tilepuzzle import generate_sliding_puzzle_problem as generate_tilepuzzle_problem
+
 
 # GENS is a dictionary mapping problem names to their respective generation functions.
 PROBLEM_GENERATORS : dict[str, Callable[[Any], None]] = {
@@ -16,5 +18,6 @@ PROBLEM_GENERATORS : dict[str, Callable[[Any], None]] = {
     "flow": generate_flow_problem,
     "bookseller": generate_bookseller_problem,
     "hiking": generate_hiking_problem,
-    "keygrid": generate_keygrid_problem
+    "keygrid": generate_keygrid_problem,
+    "tilepuzzle" : generate_tilepuzzle_problem
 }
