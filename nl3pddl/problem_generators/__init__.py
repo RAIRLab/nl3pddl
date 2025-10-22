e6950f4bc3f4b736c3a149b9ba7d1d266e8a2b69
VDrag457 <dragovicvasilije@gmail.com>
2025-10-22 13:13:39 -0400

    new branch "pacman--63"

--- nl3pddl/problem_generators/__init__.py
+++ nl3pddl/problem_generators/__init__.py
@@ -7,6 +7,7 @@ from .flow import generate_problem as generate_flow_problem
 from .bookseller import generate_problem as generate_bookseller_problem
 from .hiking import generate_hiking_problem
 from .keygrid import generate_keygrid_problem
+from .keygrid import generate_pacman_63_problem
 
 # GENS is a dictionary mapping problem names to their respective generation functions.
 PROBLEM_GENERATORS : dict[str, Callable[[Any], None]] = {
@@ -16,5 +17,6 @@ PROBLEM_GENERATORS : dict[str, Callable[[Any], None]] = {
     "flow": generate_flow_problem,
     "bookseller": generate_bookseller_problem,
     "hiking": generate_hiking_problem,
-    "keygrid": generate_keygrid_problem
+    "keygrid": generate_keygrid_problem,
+    "pacman-63": generate_pacman_63_problem
 }
